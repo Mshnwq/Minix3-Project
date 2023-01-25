19e4199933ae09517489010452eb20ae640b1322
Mshnwq <hayanx1000@gmail.com>
2023-01-25 17:37:23 +0300

    main done

--- Code/CPSimulator.c
+++ Code/CPSimulator.c
@@ -3,11 +3,10 @@
 #include <stdbool.h>
 #include <unistd.h>
 #include <pthread.h>
+#include <signal.h>
 #include <time.h>
 #include <SDL2/SDL.h>
 #include <SDL2/SDL_image.h>
-// #include "Queue.h"
-// #include "Car.h"
 #include "CarPark.h"
 
 #define PSIZE_MIN   12			// Minimum capacity of the parking space
@@ -39,7 +38,16 @@ double expnum;
 double expnum_arg;
 // Car** car_park;
 // Queue* queue;
+
+pthread_t *in_valet_threads_id;
+pthread_t *out_valet_threads_id;
+pthread_t monitor_thread_id;
+pthread_t car_gen_thread_id;
+
 pthread_mutex_t park_lock;
+pthread_cond_t arrival_cond;
+pthread_cond_t departure_cond;
+Car** parking_array;
 
 /* Statistical Variables
 *
@@ -56,30 +64,149 @@ long spt= 0; 	// Running sum of car-parking durations
 double ut = 0; 	// Current car-park space utilization
 
 /* Time Variables
-* 
+* TODO
 * 	These variables will be used to print the times in the simulator's final report.
-*/
+*/ 
 time_t current_t;			// The current time of each loop in the monitor thread
 time_t start_t;				// Time of simulator start
 time_t received_sgnl_t;		// Time of received shutdown signal
-time_t shtdown_t;			// Time the car park is shut down
-time_t val_leav_t;			// Time of leaving the valets
-time_t val_lft_t;			// Time of left the valets
+time_t shutdown_t;			// Time the car park is shut down
+time_t val_leave_t;			// Time of leaving the valets
+time_t val_left_t;			// Time of left the valets
 time_t monitor_exit_t;		// Time of left the monitor
 time_t stop_t;				// Simulator end time
 
-void* in_valet_func(void*);
 void* out_valet_func(void*);
-void* car_arrival_func(void*);
+void* in_valet_func(void*);
+void testCP();
+int poissonRandom(double);
 
-/* Signal handling */
-static volatile int stop = 0;
-/* An event handler to stop the program when pressing ctrl^c */ 
-void handle(int sig){
-	received_sgnl_t = time(NULL);
-	stop = 1;
-	shtdown_t = time(NULL);
+void* car_gen_func(void*);
+void* monitor_func(void*);
+
+/* Thread function for the monitor */
+void* monitor_func(void* arg) {
+    while (1) {
+        // Wait for a random period before checking the status of the car park
+        // sleep(rand() % 10 + 1);
+        sleep(2);
+        // Acquire the lock
+        pthread_mutex_lock(&park_lock);
+        // Print and display the status of the car park
+        printf("Spots: []\n");
+        // Update the graphical display
+        // show();
+        // Release the lock 
+        pthread_mutex_unlock(&park_lock);
+    }
 }
+
+void PrintStatistics() {
+
+	printf("\nSimulator started at:\t\t%s", ctime(&start_t));
+	printf("  Park Space Capacity was:\t%d\n", psize);
+	printf("  Allowed queue length was:\t%d\n", qsize);
+	printf("  Number of in valets was:\t%d\n", inval);
+	printf("  Number of out valets was:\t%d\n", outval);
+	printf("  Expected arrivals was:\t%.2f\n", expnum);
+	printf("Simulator stopped at:\t\t%s", ctime(&stop_t));
+
+	printf("\nCP Simulation was executed for:\t%ld seconds\n", stop_t - start_t);
+	printf("Total number of cars processed:\t%d cars\n", nc+rf);
+	printf("  Number of cars that parked:\t%d cars\n", pk);
+	printf("  Number of cars turned away:\t%d cars\n", rf);
+	printf("  Number of cars in transit:\t%d cars\n", nc-pk-Qsize());
+	printf("  Number of cars still queued:\t%d cars\n", Qsize());
+	// printf("  Number of cars still parked:\t%d cars\n\n", PQsize());
+	
+	printf("Average queue waiting time:\t\t%.2f seconds\n", (float) sqw/pk);
+	// printf("Average parking time:\t\t%.2f seconds\n", (float) spt/(pk-PQsize()));
+	printf("Percentage of park utilization:\t%.2f%%\n\n", ut);
+}
+
+void cancel_threads() {
+
+    for(int i = 0; i< inval; i++){
+		pthread_cancel(in_valet_threads_id[i]);
+	}
+
+    for(int i = 0; i < outval; i++){
+		pthread_cancel(out_valet_threads_id[i]);
+	}
+
+    pthread_cancel(monitor_thread_id);
+    pthread_cancel(car_gen_thread_id);
+    printf("CANCELED");
+}
+
+/* Signal handler for SIGTERM */
+void sigterm_handler(int signo) {
+    received_sgnl_t = time(NULL);
+    stop_t = time(NULL);
+	val_leave_t = time(NULL);
+    // char* current_time = ctime(&received_sgnl_t);
+    printf("\b\b%s:\t Received shutdown shut down signal..\n", strtok(ctime(&received_sgnl_t), "\n"));
+    // printf("%s:\t Car park is shutting down..\n", current_time);
+    printf("%s:\t The valets are leaving..\n", strtok(ctime(&val_leave_t), "\n"));
+    cancel_threads();
+    printf("%s:\t Done. %d valets left.\n", strtok(ctime(&val_leave_t), "\n"), inval+outval);
+    // printf("%s:\t Monitor exiting ...\n", current_time);
+    
+    // Shutdown graphical system 
+    finish();
+
+    /** Calculate and print statistics **/
+    PrintStatistics();
+
+    // Cleanup and exit
+    Qfree();
+    // Free memory and destroy the lock
+    free(parking_array);
+    pthread_mutex_destroy(&park_lock);
+    pthread_cond_destroy(&arrival_cond);
+    pthread_cond_destroy(&departure_cond);
+
+    exit(0);
+}
+
+/* Thread function for in-valets */
+void* in_valet_func(void* arg) {
+    while (1) {
+        /* Wait for a car to arrive */
+        pthread_mutex_lock(&park_lock);
+        while (1 == 0) {
+            pthread_cond_wait(&arrival_cond, &park_lock);
+        }
+        /* Park the car */
+        // car_park.num_arrivals--;
+        // car_park.num_available--;
+        pthread_mutex_unlock(&park_lock);
+        /* Pause for a random period before and after parking the car */
+        sleep(rand() % 1000 + 1);
+        /* Pause for a random period in the critical section */
+        usleep(rand() % 200000 + 1);
+    }
+}
+
+/* Thread function for out-valets */
+void* out_valet_func(void* arg) {
+    while (1) {
+        /* Wait for a car to arrive */
+        pthread_mutex_lock(&park_lock);
+        while (1 == 0) {
+            pthread_cond_wait(&arrival_cond, &park_lock);
+        }
+        /* Park the car */
+        // car_park.num_arrivals--;
+        // car_park.num_available--;
+        pthread_mutex_unlock(&park_lock);
+        /* Pause for a random period before and after parking the car */
+        sleep(rand() % 1000 + 1);
+        /* Pause for a random period in the critical section */
+        usleep(rand() % 200000 + 1);
+    }
+}
+
 
 int main(int argc, char* argv[]) {
 
@@ -145,72 +272,248 @@ int main(int argc, char* argv[]) {
     printf("Queue size: %d\n", qsize);
     printf("Expected number of cars: %f\n", expnum);
     
-    /* Init. signal handler to abort the program when wanted */
-	// signal(SIGINT, handle);
-
     // Initialize the car park and its components
-    Car** park = malloc(psize * sizeof(Car*));
-    pthread_mutex_t park_lock;
+    parking_array = malloc(psize * sizeof(Car*));
+    // int parkings_size;
+    // parking_array = PQiterator(&parkings_size);
     pthread_mutex_init(&park_lock, NULL);
+    pthread_cond_init(&arrival_cond, NULL);
+    pthread_cond_init(&departure_cond, NULL);  
 
-    // Qinit(qsize);
-    G2DInit(park, psize, inval, outval, park_lock);
-    // show();
+    Qinit(qsize);
+    printf("Queue initialized with capacity of %d\n", Qcapacity());
+    G2DInit(parking_array, psize, inval, outval, park_lock);
+    show();
+    sleep(1);
+
+    // Set up the signal handler for SIGTERM to abort program
+    signal(SIGTERM, sigterm_handler);
+    signal(SIGINT, sigterm_handler);
+
+    // Get the default attributes for threads
+	pthread_attr_t attr;
+    pthread_attr_init(&attr);
+
+    // Create the monitor thread
+    pthread_t monitor_thread;
+    pthread_create(&monitor_thread, &attr, monitor_func, NULL);
+    monitor_thread_id = monitor_thread;
 
     // Create and start the valet threads
-    // pthread_t in_valet_threads[inval];
-    // for(int i = 0; i < inval; i++) {
-    //     pthread_create(&in_valet_threads[i], NULL, inValet, (void*)i);
-    // }
-    // pthread_t out_valet_threads[outval];
-    // for(int i = 0; i < outval; i++) {
-    //     pthread_create(&out_valet_threads[i], NULL, outValet, (void*)i);
-    // }
+    pthread_t in_valet_threads[inval];
+    for(int i = 0; i < inval; i++) {
+        pthread_create(&in_valet_threads[i], &attr, in_valet_func, NULL);
+    }
+    in_valet_threads_id = in_valet_threads;
 
-    // // Create the car generator thread
-    // pthread_t car_gen;
-    // pthread_create(&car_gen, NULL, carGenerator, (void*)EXP_CARS);
+    pthread_t out_valet_threads[outval];
+    for(int i = 0; i < outval; i++) {
+        pthread_create(&out_valet_threads[i], &attr, out_valet_func, NULL);
+    }
+    out_valet_threads_id = out_valet_threads;
 
-    // // Wait for all threads to finish
-    // for(int i = 0; i < inval; i++) {
-    //     pthread_join(in_valet_threads[i], NULL);
-    // }
-    // for(int i = 0; i < outval; i++) {
-    //     pthread_join(out_valet_threads[i], NULL);
-    // }
-    // pthread_join(car_gen, NULL);
+    // Create the car generator thread
+    pthread_t car_gen_thread;
+    pthread_create(&car_gen_thread, &attr, car_gen_func, NULL);
+    car_gen_thread_id = car_gen_thread;
 
-    // Wait for the user to close the window
-    SDL_Event event;
-    int i = 0;
-    while (i != 6) {
-        // float rd = (rand() % 50)/100.0;
-        // sleep(rd);
-        printf("HI!%d\n", i);
-        show();
-		sleep(1);
-        i++;
-        if (SDL_PollEvent(&event) && event.type == SDL_QUIT) {
-            printf("CLODS");
-            i = 6;
-            break;
-        }
+
+	// stop_t = time(NULL);
+	// val_leav_t = time(NULL);
+    while (1) {
+
+        sleep(5);
+        printf("HHHH\n");
+        // Generate an incoming car with a Poisson distribution 
+        // double arrival_time = -logf(1.0f - (float)rand() / RAND_MAX) / arrival_rate;
+        // sleep((int)arrival_time);
+        // Add the car to the arrival queue 
+        // pthread_mutex_lock(&park_lock);
+        // if (car_park.num_arrivals < num_spots) {
+        // car_park.num_arrivals++;
+        // pthread_cond_signal(&arrival_cond);
+        // } else {
+        // / Refuse service and send the car away */
+        // }
+        // pthread_mutex_unlock(&park_lock);
     }
 
-    printf("HI!");
+
+    printf("CRASH!");
+    // return 0;
+    // testCP();
+}
+
+void* car_gen_func(void* arg) {
+    /* Start the simulation */ 
+	start_t = time(NULL);
+
+	// /* Main Thread
+	// * 	Entering an endless loop until receiving the signal.
+	// *	
+	// *	It will constantly create cars and enter them into the 
+	// *	arrival queue if it is not full.
+	// */
+    // // Enter an endless loop where it generates incoming cars 
+	while(1) {
+        // testCP();
+		int num_cars = newCars((double)1);		
+		// int num_cars = 5;		
+        printf("num_cars: %d\n", num_cars);// get a random number with Poisson distribution
+        show();
+		for(int i = 0; i < num_cars; i++) {
+            // printf("%d", i);
+			Car *car = (Car*) malloc(sizeof(Car));	// Allocate memory for new car	// Allocate memory for new car
+			// pthread_mutex_lock(&Qlock);  // Lock the arrival queue
+			if (!QisFull()){
+				CarInit(car);	// init. a car 
+				nc++;			// this car is allowed to park
+				Qenqueue(car);	// the car enqueued
+            } else {
+				rf++;			// this car is not allowed to park
+			}
+			float rd = (rand() % 20)/100.0;	// get a random value between 0 and 0.2
+			updateStats(oc, nc, pk, rf, nm, sqw, spt, ut);
+			sleep(rd);
+		    show();
+		}
+        // show();
+		sleep(1);
+	}/* End of simulation*/
+    // exit(0);
+}
+
+void testCP() {
+    // Create a car and enqueue it
+    Car car1;
+    CarInit(&car1);
+    Qenqueue(&car1);
+    printf("Car enqueued. Queue size: %d\n", Qsize());
+
+    show();
+    sleep(1);
+
+    // Create another car and enqueue it
+    Car car2;
+    CarInit(&car2);
+    Qenqueue(&car2);
+    printf("Car enqueued. Queue size: %d\n", Qsize());
+
+    show();
+    sleep(1);
+
+    // Peek at the head of the queue
+    Car* car = Qpeek();
+    printf("Car at the head of the queue: ID=%d, Arrival Time=%ld, Leave Time=%ld, Image Name=%s\n", car->cid, car->atm, car->ltm, car->pn);
+
+    show();
+    sleep(1);
+
+    // Serve the head of the queue
+    car = Qserve();
+    printf("Car served. Queue size: %d\n", Qsize());
+
+    show();
+    sleep(1);
+
+    // Create another car and enqueue it
+    Car car3;
+    CarInit(&car3);
+    Qenqueue(&car3);
+    printf("Car enqueued. Queue size: %d\n", Qsize());
+
+    show();
+    sleep(1);
+
+    // Create another car and enqueue it
+    Car car4;
+    CarInit(&car4);
+    Qenqueue(&car4);
+    printf("Car enqueued. Queue size: %d\n", Qsize());
+
+    show();
+    sleep(1);
+
+    // Check if the queue is full
+    if (QisFull()) {
+        printf("Queue is full\n");
+    } else {
+        printf("Queue is not full\n");
+    }
+
+    sleep(1);
+    show();
+
+    // Get an iterator for the queue
+    int size;
+    Car** iterator = Qiterator(&size);
+    printf("Queue contents: \n");
+    for (int i = 0; i < size; i++) {
+        printf("ID=%d, Arrival Time=%ld, Leave Time=%ld, Image Name=%s\n", iterator[i]->cid, iterator[i]->atm, iterator[i]->ltm, iterator[i]->pn);
+    }
+
+    sleep(1);
+    show();
+
+    // Clear the queue
+    Qclear();
+    printf("Queue cleared. Queue size: %d\n", Qsize());
+
+    sleep(1);
+    show();
+
+    // Check if the queue is empty
+    if (QisEmpty()) {
+        printf("Queue is empty\n");
+    } else {
+        printf("Queue is not empty\n");
+    }
+
+    // Wait for the user to close the window
+    // SDL_Event event;
+    // int i = 0;
+    // while (i != 5) {
+    //     // float rd = (rand() % 50)/100.0;
+    //     // sleep(rd);
+    //     printf("HI!%d\n", i);
+    //     show();
+	// 	sleep(1);
+    //     i++;
+    //     if (SDL_PollEvent(&event) && event.type == SDL_QUIT) {
+    //         printf("CLODS");
+    //         i = 3;
+    //         break;
+    //     }
+    // }
+
+    // printf("END!");
 
 
     /** Calculate and print statistics **/
+    // PrintStatistics();
+
     // Cleanup and exit
 
     // Free memory and destroy the lock
-    Qfree();
-    free(park);
-    pthread_mutex_destroy(&park_lock);
+    // Qfree();
+    // finish();
+    // free(parking_array);
+    // pthread_mutex_destroy(&park_lock);
 
-    return 0;
+    // exit(0);
 }
 
 
-
-
+int poissonRandom(double expectedValue) {
+    printf("ddddd%f", expectedValue);
+    int n = 0; //counter of iteration
+    double limit; 
+    double x;  //pseudo random number
+    limit = exp(-expectedValue);
+    x = rand(); 
+    while (x > limit) {
+    n++;
+    x *= rand();
+    }
+    return n;
+}
