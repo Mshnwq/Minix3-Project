c681087e7e5e97bb7e4ad004161e4778c9e8cc24
Mshnwq <hayanx1000@gmail.com>
2023-02-07 15:35:40 +0300

    added Q

--- Code/Queue.c
+++ Code/Queue.c
@@ -1,89 +1,92 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include "Queue.h"
-// #ifndef Car_H
-// #define Car_H
+#ifndef Car_H
+#define Car_H
 
-void Qinit(int n){
-	// carsQ.capacity = n;
-	// carsQ.count = 0;
-	// carsQ.head = 0;
-	// carsQ.tail = n-1;
-	// carsQ.data =(Car**) calloc(n, sizeof(Car*));
-	// carsQ.list =(Car**) calloc(0, sizeof(Car*));	
+Queue q;
+pthread_mutex_t lock;
+
+void Qinit(int n) {
+    q.data = (Car**) malloc(n * sizeof(Car*));
+    q.list = (Car**) malloc(n * sizeof(Car*));
+    q.capacity = n;
+    q.count = 0;
+    q.tail = 0;
+    q.head = 0;
+    pthread_mutex_init(&lock, NULL);
 }
 
-
-int Qcapacity(){
-	// return carsQ.capacity;
+void Qfree() {
+    free(q.data);
+    free(q.list);
+    pthread_mutex_destroy(&lock);
 }
 
-
-int Qsize(){
-	// return carsQ.count;
+void Qclear() {
+    pthread_mutex_lock(&lock);
+    q.count = 0;
+    q.tail = 0;
+    q.head = 0;
+    pthread_mutex_unlock(&lock);
 }
 
-
-void Qfree(){
-    // carsQ.count = 0;
-	// free(carsQ.data);
-	// free(carsQ.list);
+void Qenqueue(Car *car) {
+    pthread_mutex_lock(&lock);
+    if (q.count < q.capacity) {
+        q.data[q.tail] = car;
+        q.tail = (q.tail + 1) % q.capacity;
+        q.count++;
+    }
+    pthread_mutex_unlock(&lock);
 }
 
-
-void Qclear(){
-	// carsQ.count = 0;
-	// carsQ.head = 0;
-	// carsQ.tail = Qcapacity()-1;
-	// carsQ.list =(Car**) realloc(carsQ.list, Qsize()*sizeof(Car*));
+Car* Qserve() {
+    Car* car = NULL;
+    pthread_mutex_lock(&lock);
+    if (q.count > 0) {
+        car = q.data[q.head];
+        q.head = (q.head + 1) % q.capacity;
+        q.count--;
+    }
+    pthread_mutex_unlock(&lock);
+    return car;
 }
 
-
-bool QisEmpty(){
-	// return carsQ.count == 0;
+Car* Qpeek() {
+    Car* car = NULL;
+    pthread_mutex_lock(&lock);
+    if (q.count > 0) {
+        car = q.data[q.head];
+    }
+    pthread_mutex_unlock(&lock);
+    return car;
 }
 
-
-bool QisFull(){
-	// return carsQ.count == Qcapacity();
+Car** Qiterator(int *sz) {
+    int i;
+    pthread_mutex_lock(&lock);
+    for (i = 0; i < q.count; i++) {
+        q.list[i] = q.data[(q.head + i) % q.capacity];
+    }
+    *sz = q.count;
+    pthread_mutex_unlock(&lock);
+    return q.list;
 }
 
-
-void Qenqueue(Car *car){
-	// if (!QisFull()){
-	// 	carsQ.tail = (carsQ.tail+1)%Qcapacity();
-	// 	carsQ.data[carsQ.tail] = car;
-	// 	carsQ.count++;
-	// 	carsQ.list =(Car**) realloc(carsQ.list, Qsize()*sizeof(Car*));
-	// 	carsQ.list[Qsize()-1] = car;
-	// }
+int Qcapacity() {
+    return q.capacity;
 }
 
-
-Car* Qserve(){
-	// if (!QisEmpty()){
-	// 	Car *temp = carsQ.data[carsQ.head];
-	// 	carsQ.head = (carsQ.head+1)%Qcapacity();
-	// 	//carsQ.list[0] = carsQ.list[Qsize()-1];
-	// 	carsQ.count--;
-	// 	for (int i = 0; i < Qsize(); i++)
-	// 		carsQ.list[i] = carsQ.list[i+1];
-	// 	carsQ.list =(Car**) realloc(carsQ.list, Qsize()*sizeof(Car*));
-	// 	return temp;
-	// }
-	// return NULL;
+int Qsize() {
+    return q.count;
 }
 
-Car* Qpeek(){
-	// if (!QisEmpty()){
-	// 	return carsQ.data[carsQ.head];
-	// }
-	// return NULL;
+bool QisFull() {
+    return q.count == q.capacity;
 }
 
-
-Car** Qiterator(int *sz){
-	// *sz = Qsize();
-	// return carsQ.list;	
+bool QisEmpty() {
+    return q.count == 0;
 }
-// #endif
+#endif
